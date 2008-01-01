64ed54f5d878875fb0663ac826b85b31d01cd5f0
jonathan.buchanan <jonathan.buchanan@7eea2103-5a42-0410-ac78-6383eb04bdbf>
2008-01-01 02:16:29 +0000

    Implemented reparenting when a child node is assigned a parent in a different tree
    git-svn-id: http://django-mptt.googlecode.com/svn/trunk@11 7eea2103-5a42-0410-ac78-6383eb04bdbf

--- mptt/tests/__init__.py
+++ mptt/tests/__init__.py
@@ -79,7 +79,7 @@ r"""
 Test that trees are in the appropriate state and that reparented items have the
 correct tree attributes defined, should they be required for use after a save.
 
-# Extract new root node with descendants
+# Extract new root node from a subtree
 >>> platformer = Genre.objects.get(pk=platformer.pk)
 >>> platformer.parent = None
 >>> platformer.save()
@@ -95,7 +95,7 @@ correct tree attributes defined, should they be required for use after a save.
 4 2 3 1 4 5
 5 2 3 1 6 7
 
-# Extract new root node without descendants, which has existing siblings
+# Extract new root node from a leaf node which has siblings
 >>> platformer_3d = Genre.objects.get(pk=platformer_3d.pk)
 >>> platformer_3d.parent = None
 >>> platformer_3d.save()
@@ -126,7 +126,7 @@ Traceback (most recent call last):
     ...
 InvalidParent: A root node may not have its parent changed to any node in its own tree.
 
-# Make a root node a child node
+# Make a tree a subtree of another tree
 >>> rpg = Genre.objects.get(pk=rpg.pk)
 >>> platformer = Genre.objects.get(pk=platformer.pk)
 >>> rpg.parent = platformer
@@ -143,34 +143,71 @@ InvalidParent: A root node may not have its parent changed to any node in its ow
 8 6 3 2 9 10
 4 - 4 0 1 2
 
+# Move a leaf node to another tree
+>>> arpg = Genre.objects.get(pk=arpg.pk)
+>>> platformer_3d = Genre.objects.get(pk=platformer_3d.pk)
+>>> arpg.parent = platformer_3d
+>>> arpg.save()
+>>> print_tree_details([arpg])
+7 4 4 1 2 3
+>>> print_tree_details(Genre.tree.all())
+1 - 1 0 1 2
+2 - 3 0 1 10
+3 2 3 1 2 3
+5 2 3 1 4 5
+6 2 3 1 6 9
+8 6 3 2 7 8
+4 - 4 0 1 4
+7 4 4 1 2 3
+
+# Move a subtree to another tree
+>>> rpg = Genre.objects.get(pk=rpg.pk)
+>>> platformer_3d = Genre.objects.get(pk=platformer_3d.pk)
+>>> rpg.parent = platformer_3d
+>>> rpg.save()
+>>> print_tree_details([rpg])
+6 4 4 1 4 7
+>>> print_tree_details(Genre.tree.all())
+1 - 1 0 1 2
+2 - 3 0 1 6
+3 2 3 1 2 3
+5 2 3 1 4 5
+4 - 4 0 1 8
+7 4 4 1 2 3
+6 4 4 1 4 7
+8 6 4 2 5 6
+
 # Deletion ####################################################################
 
 # Delete a node which has siblings
->>> platformer_3d = Genre.objects.get(pk=platformer_3d.pk)
->>> platformer_3d.delete()
+>>> platformer_2d = Genre.objects.get(pk=platformer_2d.pk)
+>>> platformer_2d.delete()
 >>> print_tree_details(Genre.tree.all())
 1 - 1 0 1 2
-2 - 3 0 1 12
-3 2 3 1 2 3
-5 2 3 1 4 5
-6 2 3 1 6 11
-7 6 3 2 7 8
-8 6 3 2 9 10
+2 - 3 0 1 4
+5 2 3 1 2 3
+4 - 4 0 1 8
+7 4 4 1 2 3
+6 4 4 1 4 7
+8 6 4 2 5 6
 
 # Delete a node which has descendants
 >>> rpg = Genre.objects.get(pk=rpg.pk)
 >>> rpg.delete()
 >>> print_tree_details(Genre.tree.all())
 1 - 1 0 1 2
-2 - 3 0 1 6
-3 2 3 1 2 3
-5 2 3 1 4 5
+2 - 3 0 1 4
+5 2 3 1 2 3
+4 - 4 0 1 4
+7 4 4 1 2 3
 
 # Delete a root node
 >>> platformer = Genre.objects.get(pk=platformer.pk)
 >>> platformer.delete()
 >>> print_tree_details(Genre.tree.all())
 1 - 1 0 1 2
+4 - 4 0 1 4
+7 4 4 1 2 3
 """
 
 # TODO Fixtures won't work with Django MPTT unless the pre_save signal
