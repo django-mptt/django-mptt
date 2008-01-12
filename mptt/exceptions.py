547cddd88f24266e8ee8567619a9ff56cdefb9b8
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2008-01-12 03:57:04 +0000

    An exception is now raised if you attempt to set the same model up for MTTP twice; added a note to the model docs about treeify and syncdb; tidied up docs and some naming conventions elsewhere
    git-svn-id: https://django-mptt.googlecode.com/svn/trunk@76 7eea2103-5a42-0410-ac78-6383eb04bdbf

--- mptt/exceptions.py
+++ mptt/exceptions.py
@@ -9,11 +9,3 @@ class InvalidMove(Exception):
     For example, attempting to make any node a sibling of a root node.
     """
     pass
-
-class InvalidTarget(Exception):
-    """
-    An invalid node was specified as a target for a node move.
-
-    For example, passing a non-root node where a root node was expected.
-    """
-    pass
