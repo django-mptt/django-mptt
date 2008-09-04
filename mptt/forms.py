4a94596cad22600f3a90e6f199b1f54a7ed37638
jonathan.buchanan <jonathan.buchanan@7eea2103-5a42-0410-ac78-6383eb04bdbf>
2008-09-04 14:29:39 +0000

    Newforms is dead, long live forms.
    git-svn-id: http://django-mptt.googlecode.com/svn/trunk@112 7eea2103-5a42-0410-ac78-6383eb04bdbf

--- mptt/forms.py
+++ mptt/forms.py
@@ -1,9 +1,9 @@
 """
 Form components for working with trees.
 """
-from django import newforms as forms
-from django.newforms.forms import NON_FIELD_ERRORS
-from django.newforms.util import ErrorList
+from django import forms
+from django.forms.forms import NON_FIELD_ERRORS
+from django.forms.util import ErrorList
 from django.utils.translation import ugettext_lazy as _
 
 from mptt.exceptions import InvalidMove
