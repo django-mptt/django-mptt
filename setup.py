d16393116fd578488f25e6cb970aeaaf6b1adfc4
Craig de Stigter <craig.destigter@koordinates.com>
2010-09-12 21:18:28 +1200

    remove Django from install_requires. Closes gh-3

--- setup.py
+++ setup.py
@@ -13,9 +13,6 @@ setup(
     author = 'Jonathan Buchanan',
     author_email = 'craig.ds@gmail.com',
     url = 'http://github.com/django-mptt/django-mptt',
-    install_requires=[
-        'Django >= 1.1',
-    ],
     test_suite="mptt.tests.test_runner.run_tests",
     packages=find_packages(exclude=['mptt.tests']),
     classifiers = ['Development Status :: 4 - Beta',
