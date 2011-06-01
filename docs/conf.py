d05371254d4bd12dcbf2cf688296cd9aaf7f5267
Craig de Stigter <craig.destigter@koordinates.com>
2011-06-02 09:18:55 +1200

    fix docs conf and source package, thanks @cans . fixes #128

--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,8 @@ import sys, os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
 
 # -- General configuration -----------------------------------------------------
 
