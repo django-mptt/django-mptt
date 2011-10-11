72985050ba47b3b483c882c2683cb2a58f0f758c
Carlos Palol <carlos.palol@awarepixel.com>
2011-10-11 14:59:34 +0200

    Updated MPTTModelAdmin change list to match Django 1.3.1 admin. Fixes the overlapping filters div over the table.

--- mptt/templatetags/mptt_admin.py
+++ mptt/templatetags/mptt_admin.py
@@ -1,5 +1,7 @@
 from django.conf import settings
-from django.contrib.admin.views.main import EMPTY_CHANGELIST_VALUE
+from django.contrib.admin.util import lookup_field, display_for_field, label_for_field
+from django.contrib.admin.views.main import (ALL_VAR, EMPTY_CHANGELIST_VALUE,
+    ORDER_VAR, ORDER_TYPE_VAR, PAGE_VAR, SEARCH_VAR)
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils import dateformat
@@ -19,12 +21,16 @@ register = Library()
 MPTT_ADMIN_LEVEL_INDENT = getattr(settings, 'MPTT_ADMIN_LEVEL_INDENT', 10)
 
 ###
-# Ripped from contrib.admin's items_for_result tag.
+# Ripped from contrib.admin's (1.3.1) items_for_result tag.
 # The only difference is we're indenting nodes according to their level.
 def mptt_items_for_result(cl, result, form):
+    """
+    Generates the actual list of data.
+    """
     first = True
     pk = cl.lookup_opts.pk.attname
     
+    ##### MPTT ADDITION START
     # figure out which field to indent
     mptt_indent_field = getattr(cl.model_admin, 'mptt_indent_field', None)
     if not mptt_indent_field:
@@ -41,29 +47,18 @@ def mptt_items_for_result(cl, result, form):
                 # first model field, use this one
                 mptt_indent_field = field_name
                 break
+    ##### MPTT ADDITION END
     
     for field_name in cl.list_display:
         row_class = ''
-        f = None
         try:
-            f = cl.lookup_opts.get_field(field_name)
-        except models.FieldDoesNotExist:
-            # For non-field list_display values, the value is either a method,
-            # property or returned via a callable.
-            try:
-                if callable(field_name):
-                    attr = field_name
-                    value = attr(result)
-                elif hasattr(cl.model_admin, field_name) and \
-                   not field_name == '__str__' and not field_name == '__unicode__':
-                    attr = getattr(cl.model_admin, field_name)
-                    value = attr(result)
-                else:
-                    attr = getattr(result, field_name)
-                    if callable(attr):
-                        value = attr()
-                    else:
-                        value = attr
+            f, attr, value = lookup_field(field_name, result, cl.model_admin)
+        except (AttributeError, ObjectDoesNotExist):
+            result_repr = EMPTY_CHANGELIST_VALUE
+        else:
+            if f is None:
+                if field_name == u'action_checkbox':
+                    row_class = ' class="action-checkbox"'
                 allow_tags = getattr(attr, 'allow_tags', False)
                 boolean = getattr(attr, 'boolean', False)
                 if boolean:
@@ -71,59 +66,35 @@ def mptt_items_for_result(cl, result, form):
                     result_repr = _boolean_icon(value)
                 else:
                     result_repr = smart_unicode(value)
-            except (AttributeError, ObjectDoesNotExist):
-                result_repr = EMPTY_CHANGELIST_VALUE
-            else:
                 # Strip HTML tags in the resulting text, except if the
                 # function has an "allow_tags" attribute set to True.
                 if not allow_tags:
                     result_repr = escape(result_repr)
                 else:
                     result_repr = mark_safe(result_repr)
-        else:
-            field_val = getattr(result, f.attname)
-
-            if isinstance(f.rel, models.ManyToOneRel):
-                if field_val is not None:
-                    result_repr = escape(getattr(result, f.name))
-                else:
-                    result_repr = EMPTY_CHANGELIST_VALUE
-            # Dates and times are special: They're formatted in a certain way.
-            elif isinstance(f, models.DateField) or isinstance(f, models.TimeField):
-                if field_val:
-                    (date_format, datetime_format, time_format) = get_date_formats()
-                    if isinstance(f, models.DateTimeField):
-                        result_repr = capfirst(dateformat.format(field_val, datetime_format))
-                    elif isinstance(f, models.TimeField):
-                        result_repr = capfirst(dateformat.time_format(field_val, time_format))
-                    else:
-                        result_repr = capfirst(dateformat.format(field_val, date_format))
-                else:
-                    result_repr = EMPTY_CHANGELIST_VALUE
-                row_class = ' class="nowrap"'
-            # Booleans are special: We use images.
-            elif isinstance(f, models.BooleanField) or isinstance(f, models.NullBooleanField):
-                result_repr = _boolean_icon(field_val)
-            # DecimalFields are special: Zero-pad the decimals.
-            elif isinstance(f, models.DecimalField):
-                if field_val is not None:
-                    result_repr = ('%%.%sf' % f.decimal_places) % field_val
-                else:
-                    result_repr = EMPTY_CHANGELIST_VALUE
-            # Fields with choices are special: Use the representation
-            # of the choice.
-            elif f.flatchoices:
-                result_repr = dict(f.flatchoices).get(field_val, EMPTY_CHANGELIST_VALUE)
             else:
-                result_repr = escape(field_val)
+                if isinstance(f.rel, models.ManyToOneRel):
+                    field_val = getattr(result, f.name)
+                    if field_val is None:
+                        result_repr = EMPTY_CHANGELIST_VALUE
+                    else:
+                        result_repr = escape(field_val)
+                else:
+                    result_repr = display_for_field(value, f)
+                if isinstance(f, models.DateField)\
+                or isinstance(f, models.TimeField)\
+                or isinstance(f, models.ForeignKey):
+                    row_class = ' class="nowrap"'
         if force_unicode(result_repr) == '':
             result_repr = mark_safe('&nbsp;')
         
+        ##### MPTT ADDITION START
         if field_name == mptt_indent_field:
             level = getattr(result, result._mptt_meta.level_attr)
             padding_attr = ' style="padding-left:%spx"' % (5 + MPTT_ADMIN_LEVEL_INDENT * level)
         else:
             padding_attr = ''
+        ##### MPTT ADDITION END
         
         # If list_display_links not defined, add the link tag to the first field
         if (first and not cl.list_display_links) or field_name in cl.list_display_links:
@@ -138,19 +109,25 @@ def mptt_items_for_result(cl, result, form):
                 attr = pk
             value = result.serializable_value(attr)
             result_id = repr(force_unicode(value))[1:]
+            ##### MPTT SUBSTITUTION START
             yield mark_safe(u'<%s%s%s><a href="%s"%s>%s</a></%s>' % \
                 (table_tag, row_class, padding_attr, url, (cl.is_popup and ' onclick="opener.dismissRelatedLookupPopup(window, %s); return false;"' % result_id or ''), conditional_escape(result_repr), table_tag))
+            ##### MPTT SUBSTITUTION END
         else:
             # By default the fields come from ModelAdmin.list_editable, but if we pull
             # the fields out of the form instead of list_editable custom admins
             # can provide fields on a per request basis
-            if form and field_name in form.fields:
+            if (form and field_name in form.fields and not (
+                    field_name == cl.model._meta.pk.name and
+                        form[cl.model._meta.pk.name].is_hidden)):
                 bf = form[field_name]
                 result_repr = mark_safe(force_unicode(bf.errors) + force_unicode(bf))
             else:
                 result_repr = conditional_escape(result_repr)
+            ##### MPTT SUBSTITUTION START
             yield mark_safe(u'<td%s%s>%s</td>' % (row_class, padding_attr, result_repr))
-    if form:
+            ##### MPTT SUBSTITUTION END
+    if form and not form[cl.model._meta.pk.name].is_hidden:
         yield mark_safe(u'<td>%s</td>' % force_unicode(form[cl.model._meta.pk.name]))
 
 
@@ -172,4 +149,5 @@ def mptt_result_list(cl):
             'results': list(mptt_results(cl))}
             
 # custom template is merely so we can strip out sortable-ness from the column headers
+# Based on admin/change_list_results.html (1.3.1)
 mptt_result_list = register.inclusion_tag("admin/mptt_change_list_results.html")(mptt_result_list)
