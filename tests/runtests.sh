d9077cf53878ab1fcd3a58ac1860efe91837d5f0
Craig de Stigter <craig.destigter@koordinates.com>
2012-09-21 14:59:15 +1200

    fix tests when django-admin.py doesnt exist (ubuntu package renames it to django-admin)

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -2,4 +2,10 @@
 export PYTHONPATH="./"
 export DJANGO_SETTINGS_MODULE='settings'
 
-django-admin.py test --settings=$DJANGO_SETTINGS_MODULE --pythonpath="../" "$@"
+if [ `which django-admin.py` ] ; then
+    export DJANGO_ADMIN=django-admin.py
+else
+    export DJANGO_ADMIN=django-admin
+fi
+
+$DJANGO_ADMIN test --settings=$DJANGO_SETTINGS_MODULE --pythonpath="../" "$@"
