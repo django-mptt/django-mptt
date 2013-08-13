03a2e2bdf43fc5abb6be1ecdd43576fd4d7cb3e9
Craig de Stigter <craig.destigter@koordinates.com>
2013-08-14 10:35:59 +1200

    update copyright notice to add me

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'django-mptt'
-copyright = '2007 - 2011, Jonathan Buchanan and others'
+copyright = '2007 - 2013, Craig de Stigter, Jonathan Buchanan and others'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
