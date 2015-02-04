ce8820fcd3cc89f65488489729d047af8a448491
Javad Karabi <karabijavad@gmail.com>
2015-02-04 17:25:38 -0600

    brings get_queryset_ancestors and get_queryset_descendants methods into TreeQuerySet as get_ancestors and get_descendants

--- mptt/querysets.py
+++ mptt/querysets.py
@@ -1,30 +1,8 @@
 from django.db import models
 
 class TreeQuerySet(models.query.QuerySet):
-    def get_descendants(self, include_self=False):
-        if include_self:
-            return self.model.objects.filter(
-                tree_id=self.values("tree_id"),
-                lft__gte=self.values("lft"),
-                rght__lte=self.values("rght"),
-            )
-        else:
-            return self.model.objects.filter(
-                tree_id=self.values("tree_id"),
-                lft__gt=self.values("lft"),
-                rght__lt=self.values("rght"),
-            )
+    def get_descendants(self, *args, **kwargs):
+        return self.model.objects.get_queryset_descendants(self, *args, **kwargs)
 
-    def get_ancestors(self, include_self=False):
-        if include_self:
-            return self.model.objects.filter(
-                tree_id=self.values("tree_id"),
-                lft__lte=self.values("lft"),
-                rght__gte=self.values("rght"),
-            )
-        else:
-            return self.model.objects.filter(
-                tree_id=self.values("tree_id"),
-                lft__lt=self.values("lft"),
-                rght__gt=self.values("rght"),
-            )
+    def get_ancestors(self, *args, **kwargs):
+        return self.model.objects.get_queryset_ancestors(self, *args, **kwargs)
