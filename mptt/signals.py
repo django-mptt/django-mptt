3d2a67ab4b46bf389376544fb21341163b57e8fa
orf <tom@tomforb.es>
2016-12-19 11:43:38 +0000

    Use ModelSignal over plain Signal

--- mptt/signals.py
+++ mptt/signals.py
@@ -1,10 +1,10 @@
-import django.dispatch
+from django.db.models.signals import ModelSignal as Signal
 
 # Behaves like Djangos normal pre-/post_save signals signals with the
 # added arguments ``target`` and ``position`` that matches those of
 # ``move_to``.
 # If the signal is called from ``save`` it'll not be pass position.
-node_moved = django.dispatch.Signal(providing_args=[
+node_moved = Signal(providing_args=[
     'instance',
     'target',
     'position'
