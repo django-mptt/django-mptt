e6651d09b7d87a37d978ca376923a902893345cf
orf <tom@tomforb.es>
2016-12-19 11:44:54 +0000

    Update signals.py

--- mptt/signals.py
+++ mptt/signals.py
@@ -1,10 +1,10 @@
-from django.db.models.signals import ModelSignal as Signal
+from django.db.models.signals import ModelSignal
 
 # Behaves like Djangos normal pre-/post_save signals signals with the
 # added arguments ``target`` and ``position`` that matches those of
 # ``move_to``.
 # If the signal is called from ``save`` it'll not be pass position.
-node_moved = Signal(providing_args=[
+node_moved = ModelSignal(providing_args=[
     'instance',
     'target',
     'position'
