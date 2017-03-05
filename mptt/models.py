3cc0bffc0f04e3333b86d43abd81dd0aaa086e0e
Olivier Dalang <olivier.dalang@gmail.com>
2017-03-05 20:12:15 +0000

    Fix #466
    Thanks @matthiask for implementation
    
    ( see https://github.com/django-mptt/django-mptt/issues/466#issuecomment-236347265 )

--- mptt/models.py
+++ mptt/models.py
@@ -805,7 +805,7 @@ class MPTTModel(six.with_metaclass(MPTTModelBase, models.Model)):
         field_names = []
         internal_fields = (
             self._mptt_meta.left_attr, self._mptt_meta.right_attr, self._mptt_meta.tree_id_attr,
-            self._mptt_meta.level_attr, self._mptt_meta.parent_attr)
+            self._mptt_meta.level_attr)
         for field in self._meta.fields:
             if (field.name not in internal_fields) and (not isinstance(field, AutoField)) and (not field.primary_key):  # noqa
                 field_names.append(field.name)
