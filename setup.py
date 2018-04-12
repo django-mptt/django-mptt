d595bc7d43771173f6addbfe7f78ecf837984110
Jon Dufresne <jon.dufresne@gmail.com>
2018-04-12 04:02:41 -0700

    Document project as stable and ready for use in production
    Helps users know, at a glance, the state of the project. For a complete
    list of trove classifiers, see:
    
    https://pypi.python.org/pypi?%3Aaction=list_classifiers

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     ],
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*",
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Framework :: Django :: 1.8',
