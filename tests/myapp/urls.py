d2a476c504392e51cd2130530f5cfbe9636e6079
Michael Käufl <michael-k@users.noreply.github.com>
2018-04-19 21:41:18 +0200

    [tests] Remove leftover code for Django < 1.11 support

--- tests/myapp/urls.py
+++ tests/myapp/urls.py
@@ -3,8 +3,4 @@ from django.conf.urls import include, url
 
 from django.contrib import admin
 
-
-if django.VERSION >= (1, 9):
-    urlpatterns = [url(r'^admin/', admin.site.urls)]
-else:
-    urlpatterns = [url(r'^admin/', include(admin.site.urls))]
+urlpatterns = [url(r'^admin/', admin.site.urls)]
