c41f599f86698645eee240fa47d3dd98f7913223
Michael Käufl <michael-k@users.noreply.github.com>
2019-02-16 13:37:33 +0100

    Add support for Django 2.1 and 2.2

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
