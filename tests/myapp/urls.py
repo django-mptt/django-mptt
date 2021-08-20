443e625549d9a961a5f0b7a7777ba90d195e6df7
Matthias Kestenholz <mk@feinheit.ch>
2021-08-20 17:48:47 +0200

    Avoid django.conf.urls.url

--- tests/myapp/urls.py
+++ tests/myapp/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import url
+from django.urls import path
 from django.contrib import admin
 
 
-urlpatterns = [url(r"^admin/", admin.site.urls)]
+urlpatterns = [path("admin/", admin.site.urls)]
